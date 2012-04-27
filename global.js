2d18168c9a56216f6882e2dc74910ea83d387685
Paul Barrick <paul@imperativedesign.net>
2012-04-27 09:55:31 -0400

    Updates to the ribbon timing
    Fixed it so it isn't animating it so frequently. Smoother looking
    animation.

--- global.js
+++ global.js
@@ -4,36 +4,46 @@ $(document).ready(function(){
 		$ribbon = $('#sidebar #ribbon'), $sidebar = $('#sidebar #inner'), side_height = $sidebar.height(),
 		scrollTop = $(window).scrollTop(), 
 		scrollBottom = $(window).scrollTop() + $(window).height(),
-		max = $(window).scrollTop() + $(window).height();
+		max = $(window).scrollTop() + $(window).height(),
+		ribbon = {};
+	
+		function doClear(){
+			clearTimeout(ribbon.timer);
+			ribbon.timer = null;
+		}
+		
+	  function checkPos(ribbon){
+		if(ribbon.sum > ribbon.sidebar_height){
+			$ribbon.clearQueue().animate({
+				marginTop: (ribbon.window_height - ribbon.ribbon_height)
+				}, 100, function() { doClear() });
+		} else {
+			$ribbon.clearQueue().animate({
+				marginTop: '10px'
+				}, 100, function() { doClear() });
+		}
+	  }
+	  function animateWindow(ribbon) {
+			ribbon.timer = setTimeout(function(){
+				$ribbon.animate({
+					top: $(window).scrollTop()
+					}, 500, function(){
+						//console.log('window height: ' + window_height + ' sidebar height ' + sidebar_height + ' ribbon height ' + ribbon_height + ' space to top ' + toTop);
+						checkPos(ribbon);
+					});
+				}, 300);
+		}
+	
 		
 	//when you scroll get the coords
 	$(window).on('scroll', function(){
-		var window_height = $(window).outerHeight(),
-					toTop = $(window).scrollTop(),
-			ribbon_height = $('#ribbon').outerHeight(),
-		   sidebar_height = $('#sidebar #inner').height(),
-					  sum = window_height + toTop;
+				ribbon.window_height = $(window).outerHeight(),
+				ribbon.toTop = $(window).scrollTop(),
+				ribbon.ribbon_height = $('#ribbon').outerHeight(),
+			    ribbon.sidebar_height = $('#sidebar #inner').height(),
+				ribbon.sum = ribbon.window_height + ribbon.toTop;
 		
-					  function checkPos(){
-	  					if(sum > sidebar_height){
-	  						console.log('greater')
-	  						$ribbon.clearQueue().animate({
-	  							marginTop: (window_height - ribbon_height)
-	  							}, 100);
-	  					} else {
-	  						$ribbon.clearQueue().animate({
-	  							marginTop: '10px'
-	  							}, 100);
-	  					}
-					  }
-					  
-			$ribbon.animate({
-				top: $(window).scrollTop()
-				}, 100, function(){
-					
-					console.log('window height: ' + window_height + ' sidebar height ' + sidebar_height + ' ribbon height ' + ribbon_height + ' space to top ' + toTop);
-					checkPos();
-				});
+				animateWindow(ribbon);	  
 	});
 });
 
